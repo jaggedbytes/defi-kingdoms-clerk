9834ab485edd7ee04a23c60713d0109903c0af5b
Bean Jackson <jaggedbytes@protonmail.com>
2022-01-02 17:59:18 -0500

    Fix cross origin error for Google Fonts

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,7 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
         <link 
-          rel="preconnect"
-          href="https://fonts.googleapis.com" 
-        />
-        <link 
-          rel="preconnect"
-          href="https://fonts.gstatic.com" crossorigin
-        />
-        <link 
-          href="https://fonts.googleapis.com/css2?family=Lora&family=Poppins&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Lora&family=Poppins&display=optional"
           rel="stylesheet"
         />
       </Head>
